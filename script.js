4c02b0bdf5e283c393031168d700dee8681e2ff5
MUH. AFRIZAL <87596110+afrizal0@users.noreply.github.com>
2022-07-25 10:18:34 +0800

    remove data folder, found a bug

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-const articleName = require('./data/data.js')
+
 const locationHref = window.location.href
 const searchParam = new URLSearchParams(window.location.search)
 
 // check if its article name is supported article
 const isValidArticleName = (name) => name.map((item) => locationHref.includes(item)).includes(true)
+
+const articleName = {
+    mediaType1: [
+        "kompas.com",
+        "kompasiana.com",
+        "tribunnews.com",
+        "viva.co.id",
+        "suara.com",
+        "jawapos.com",
+        "pikiran-rakyat.com"
+    ],
+    mediaType2: [
+        "sindonews.com"
+    ]
+};
 
 if (isValidArticleName(articleName.mediaType1) && searchParam.get('page') !== 'all') {
     searchParam.set('page', 'all')
